e3535b1f260db9d95114cd3e98180bca87225c7c
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-09-27 17:24:09 +0500

    all

--- service/case.go
+++ service/case.go
@@ -1,1 +1,21 @@
 package service
+
+import (
+	"database/sql"
+	"log/slog"
+	pb "question/genproto/question"
+	"question/storage"
+)
+
+type CaseService struct {
+	pb.UnimplementedTestCaseServiceServer
+	Case   storage.Istorage
+	Logger *slog.Logger
+}
+
+func NewCaseService(db *sql.DB, Logger *slog.Logger, istorage storage.Istorage) *CaseService {
+	return &CaseService{
+		Case:   istorage,
+		Logger: Logger,
+	}
+}
--- service/output.go
+++ service/output.go
@@ -1,1 +1,21 @@
 package service
+
+import (
+	"database/sql"
+	"log/slog"
+	pb "question/genproto/question"
+	"question/storage"
+)
+
+type OutputService struct {
+	pb.UnimplementedOutputServiceServer
+	Output storage.Istorage
+	Logger *slog.Logger
+}
+
+func NewOutputService(db *sql.DB, Logger *slog.Logger, istorage storage.Istorage) *OutputService {
+	return &OutputService{
+		Output: istorage,
+		Logger: Logger,
+	}
+}
