83a17b81d15bed7850143a9cbcb29ab08b39b50e
Sanjarbek <sanjarbekabdurahmonov123@gmail.com>
2024-10-05 16:59:01 +0500

    Ready

--- migrations/000001_create_table.up.sql
+++ migrations/000001_create_table.up.sql
@@ -14,16 +14,17 @@ CREATE TABLE subject_topics (
     subject_id UUID REFERENCES subjects(id) ON DELETE CASCADE,
     name VARCHAR NOT NULL,
     description TEXT,
-    question_count int,
+    question_count INT,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW(),
     deleted_at TIMESTAMP
 );
 
+-- Create user_tasks table
 CREATE TABLE user_tasks (
-    id UUID, 
-    user_id UUID,
-    question_id UUID, 
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,     
+    question_id UUID NOT NULL,    
     topic_id UUID REFERENCES subject_topics(id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW(),
